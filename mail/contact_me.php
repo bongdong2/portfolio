ce4af923732dfe2528497b60c827f6256f15e9b3
bongdong2 <jsu373712@naver.com>
2019-11-18 08:31:50 +0900

    아바타 수정,  이메일 메시지 수정, 소셜 버튼 수정

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -13,8 +13,8 @@ $message = strip_tags(htmlspecialchars($_POST['message']));
 // Create the email and send the message
 $to = "jsu373712@naver.com"; // Add your email address inbetween the "" replacing yourname@yourdomain.com - This is where the form will send a message to.
 $subject = "메일이 도착했습니다. :  $name";
-$body = "당신의 개인홈페이지에서 메일이 도착했습니다..\n\n"."이름: $name\n\n이메일: $email\n\n연락차: $phone\n\내용:\n$message";
-$header = "From: noreply@yourdomain.com\n"; // 메일 지원하는 개인호스팅 서버
+$body = "당신의 개인홈페이지에서 메일이 도착했습니다.\n\n"."이름: $name\n\n이메일: $email\n\n연락처: $phone\n내용:\n$message";
+$header = "From: noreply@cse2233@cafe24.com\n"; // 메일 지원하는 개인호스팅 서버
 $header .= "Reply-To: $email";	
 
 if(!mail($to, $subject, $body, $header))
